8b8235688789e77adbdd2f3b37a6cfc2be6ca11c
putrisylw <putri.syalwa.tif419@polban.ac.id>
2021-05-03 09:15:55 +0700

    Update Class GotoClass.java

--- app/src/main/java/org/thoughtcrime/securesms/conversationlist/model/GotoGoogle.java
+++ app/src/main/java/org/thoughtcrime/securesms/conversationlist/model/GotoGoogle.java
@@ -25,7 +25,7 @@ public class GotoGoogle extends View {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         getContext().startActivity(new Intent(Intent.ACTION_VIEW,
-                                Uri.parse("http://google.com" + packageName)));
+                                Uri.parse("http://google.com")));
                     }
                 })
                 .setNegativeButton("Not Now", new DialogInterface.OnClickListener() {
